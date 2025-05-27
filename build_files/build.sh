dbd919a661d0b237f0be75894321230153a4c728
bemain <bemain.dev@gmail.com>
2025-05-27 15:13:17 +0200

    Take ownership
    Remove things from the template I don't care about, get the builds working and set some basic config.

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,23 @@
 
 set -ouex pipefail
 
+mkdir /var/opt /var/roothome
+
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# Installs packages from fedora repos
+dnf5 install -y \
+	tmux \
+	NetworkManager-l2tp-gnome \
+	distrobox \
+	wireguard-tools \
+	zsh \
+	papirus-icon-theme \
+	snapper gnome-tweaks
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 remove -y toolbox
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
 
-#### Example for enabling a System Unit File
+cp /ctx/cosign.pub /etc/pki/bemain-cosign.pub
 
-systemctl enable podman.socket
+rm -rf /var/roothome/
