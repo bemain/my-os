0f87335e1c8b1747a9309a5a020450df3695d06d
bemain <bemain.dev@gmail.com>
2026-04-22 10:50:18 +0200

    Sort packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,15 +10,15 @@ dnf5 config-manager addrepo --from-repofile=https://repository.mullvad.net/rpm/s
 # Install packages I want
 dnf5 install -y \
 	NetworkManager-l2tp-gnome \
-	krb5-workstation \
-	distrobox \
-	wireguard-tools \
-	zsh \
-	papirus-icon-theme \
 	btrbk \
+	distrobox \
 	gparted \
+	krb5-workstation \
+	mullvad-vpn \
+	papirus-icon-theme \
 	waydroid \
-	mullvad-vpn
+	wireguard-tools \
+	zsh
 
 # Mullvad VPN is installed in /opt which does not persist to the final image, so we move it
 mkdir -p /usr/lib/mullvad
@@ -26,8 +26,8 @@ mv "/opt/Mullvad VPN"/* /usr/lib/mullvad/
 
 # Remove packages I don't use
 dnf5 remove -y \
-    toolbox \
-    firefox  # Prefer the Flatpak version
+	firefox \
+    toolbox
 
 
 cp /ctx/cosign.pub /etc/pki/bemain-cosign.pub  # Copy signing key
